e87f2d54ba84b49aeabfc894cc1504e0b71cccaa
Broocevelt <DANHID95@hotmail.com>
2015-11-04 15:27:24 +0100

    Most DAOImp classes

--- VehicleRent/src/persistence/CategoryDAOImp.java
+++ VehicleRent/src/persistence/CategoryDAOImp.java
@@ -1,5 +1,4 @@
 //CategoryDAOImp
-//ABLUBLUBLUB
 package persistence;
 import java.sql.*;
 import java.util.ArrayList;
---
+++ VehicleRent/src/persistence/CustomerDAOImp.java
@@ -0,0 +1,97 @@
+//CategoryDAOImp
+package persistence;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.time.LocalDateTime;
+
+import persistence.dto.CustomerDTO;
+import exceptions.DAOException;
+
+public class CustomerDAOImp implements ICustomerDAO {
+	protected ConnectionManager connManager;
+
+	public CustomerDAOImp() throws DAOException {
+		super();
+		try {
+			connManager= new ConnectionManager("vehiclerentalDB");
+			}
+		catch (ClassNotFoundException e) {
+			throw new DAOException(e);
+			}
+		}
+	
+
+	  
+	public List <CustomerDTO> getCustomers() throws DAOException {
+		try {
+			connManager.connect();
+			ResultSet rs=connManager.queryDB("select * from CUSTOMER");
+			connManager.close();
+			
+			List<CustomerDTO> customerDTOList = new ArrayList<CustomerDTO>();
+			
+			try {
+				while (rs.next()){
+					CustomerDTO cusDTO = new CustomerDTO(
+							rs.getString("DNI"),
+							rs.getString("NAMEANDSURNAME"),
+							rs.getString("ADDRESS"),
+							rs.getString("CITY"),
+							rs.getString("POSTALCODE"),
+							LocalDateTime.of(rs.getDate("DATEDRIVERLICENSE").toLocalDate(), rs.getTime("DATEDRIVERLICENSE").toLocalTime()),
+							rs.getString("DIGITSCC"),
+							rs.getInt("MONTHCC"),
+							rs.getInt("YEARCC"),
+							rs.getInt("CVCCC"),
+							rs.getString("TYPECC"));
+					
+					customerDTOList.add(cusDTO);
+					}
+				return customerDTOList;
+			}
+			catch (Exception e){throw new DAOException(e);}
+		}
+		catch (SQLException e){throw new DAOException(e);}	
+		catch (DAOException e){throw e;}
+	
+	}
+
+	  
+	public CustomerDTO findCustomer(String dni)throws DAOException{
+		try{
+			connManager.connect();
+			ResultSet rs=connManager.queryDB("select * from CUSTOMER where DNI= '"+dni+"'");
+			connManager.close();
+		
+			if (rs.next())
+				return new CustomerDTO(
+						rs.getString("DNI"),
+						rs.getString("NAMEANDSURNAME"),
+						rs.getString("ADDRESS"),
+						rs.getString("CITY"),
+						rs.getString("POSTALCODE"),
+						LocalDateTime.of(rs.getDate("DATEDRIVERLICENSE").toLocalDate(), rs.getTime("DATEDRIVERLICENSE").toLocalTime()),
+						rs.getString("DIGITSCC"),
+						rs.getInt("MONTHCC"),
+						rs.getInt("YEARCC"),
+						rs.getInt("CVCCC"),
+						rs.getString("TYPECC"));
+			else
+				return null;
+			
+		}
+		catch (SQLException e){	throw new DAOException(e);}	
+	}
+	
+	public void createCustomer(CustomerDTO c) throws DAOException{
+		try{
+			connManager.connect();
+			connManager.updateDB("INSERT INTO CUSTOMER VALUES ("+c.getDni()+","+c.getNameAndSurname()+","+c.getAddress()+","+c.getCity()+","+c.getPostalCode()+","+c.getDateDriverLicense()+","+c.getDigitsCC()+","+c.getMonthCC()+","+c.getYearCC()+","+c.getCvcCC()+","+c.getTypeCC()+")");;
+			connManager.close();
+			
+		}
+		catch (SQLException e){	throw new DAOException(e);}
+	}
+}
